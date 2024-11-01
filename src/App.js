0cfe6bf9ee077ce7d43a966343d16e0898d90193
George Karaviotis <geokaraviotis@gmail.com>
2024-11-01 17:35:34 +0200

    smth

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Chat from './components/Chat';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  // return (
-  //   <div>
-  //     <Chat />
-  //   </div>
-  // );
   return (
-    <Router basename={'https://geoka123.github.io/fhbot-front-end/'}>
-      {'/'}
-    </Router>
+    <div>
+      <Chat />
+    </div>
   );
+  // return (
+  //   <Router basename={'https://geoka123.github.io/fhbot-front-end/'}>
+  //     {'/'}
+  //   </Router>
+  // );
 }
 
 export default App;
